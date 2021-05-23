ad143a5e7d6703de0ae8928bf71f7a6abf534271
arif <arifhasnathc4u@gmail.com>
2021-05-23 15:15:49 +0600

    flash project

---
+++ Six/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Six
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
