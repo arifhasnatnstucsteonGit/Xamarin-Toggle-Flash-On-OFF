8f8cabd534e81eb2f9698833d89c7f9f605581e1
arif <arifhasnathc4u@gmail.com>
2021-05-23 15:29:56 +0600

    data aded

--- Six/MainPage.xaml.cs
+++ Six/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace Six
 {
@@ -13,12 +14,34 @@ namespace Six
         public MainPage()
         {
             InitializeComponent();
+
         }
 
-        void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+       async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
         {
+            try
+            {
+                // Turn On Flashlight  
+                await Flashlight.TurnOnAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await ShowAlert(fnsEx.Message);
+            }
+            catch (PermissionException pEx)
+            {
+                await ShowAlert(pEx.Message);
+            }
+            catch (Exception ex)
+            {
+                await ShowAlert(ex.Message);
+            }
 
+        }
 
+        public async Task ShowAlert(string message)
+        {
+            await DisplayAlert("Faild", message, "Ok");
         }
     }
 }
