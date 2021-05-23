f6445773629309295ca5cb992ea5df9ff1ffc0df
arif <arifhasnathc4u@gmail.com>
2021-05-23 17:08:22 +0600

    flash on off

--- Six/MainPage.xaml.cs
+++ Six/MainPage.xaml.cs
@@ -11,18 +11,55 @@ namespace Six
 {
     public partial class MainPage : ContentPage
     {
+
+        bool isFlashOn = false;
+
         public MainPage()
         {
             InitializeComponent();
 
         }
 
-       async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+        void ImageButton_Clicked(System.Object sender, System.EventArgs e)
         {
+            if (isFlashOn)
+            {
+                isFlashOn = !isFlashOn;
+                turnOFFflash();
+            }
+            else {
+                isFlashOn = !isFlashOn;
+                turnONflash();
+            }
+            
+
+        }
+
+        async public void turnONflash() {
             try
             {
                 // Turn On Flashlight  
                 await Flashlight.TurnOnAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await ShowAlert(fnsEx.Message);
+            }
+            catch (PermissionException pEx)
+            {
+                await ShowAlert(pEx.Message);
+            }
+            catch (Exception ex)
+            {
+                await ShowAlert(ex.Message);
+            }
+        }
+
+        async public void turnOFFflash() {
+            try
+            {
+                // Turn Off Flashlight  
+                await Flashlight.TurnOffAsync();
             }
             catch (FeatureNotSupportedException fnsEx)
             {
